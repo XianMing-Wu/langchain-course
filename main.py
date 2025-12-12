c2e470b01846c210dd7dab162e3e97e74f4233ad
wxm <18854896936@163.com>
2025-12-12 14:59:11 +0800

    Search Agent

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+# tavily import TavilyClient
+from langchain_tavily import TavilySearch
+#tavily = TavilyClient()
+
+
+llm = ChatOpenAI(model="deepseek-chat", base_url=os.getenv("OPENAI_BASE_URL"))
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
 def main():
     print("Hello from langchain-course!")
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in San Francisco?")})
+    print(result)
 
 
 if __name__ == "__main__":
