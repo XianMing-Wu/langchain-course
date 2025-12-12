83b45e6c5ac1050f1c024b3ca9189d1c9db67846
wxm <18854896936@163.com>
2025-12-12 15:20:21 +0800

    structure output

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-
+from pydantic import BaseModel, Field
+from typing import List
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -11,11 +12,24 @@ from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 # tavily = TavilyClient()
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
 
 
 llm = ChatOpenAI(model="deepseek-chat", base_url=os.getenv("OPENAI_BASE_URL"))
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
