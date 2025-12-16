8092c0db339f02fd75312140bbec0dbf5d5856f8
wxm <18854896936@163.com>
2025-12-16 09:04:02 +0800

    rag

---
+++ main.py
@@ -0,0 +1,16 @@
+import os 
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeSparseVectorStore
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print("Ingesting...")
+    loader = TextLoader("/config/workspace/langchain-course/mediumblog1.txt")
+    document = loader.load()
+    
+
